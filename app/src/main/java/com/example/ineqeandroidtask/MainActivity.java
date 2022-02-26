d82af33e2577278a6eed0030b7bc358b48cd02ad
40210069 <slivingstone07@qub.ac.uk>
2022-02-26 12:40:22 +0000

    Fixes

--- app/src/main/java/com/example/ineqeandroidtask/MainActivity.java
+++ app/src/main/java/com/example/ineqeandroidtask/MainActivity.java
@@ -23,6 +23,7 @@ import org.json.JSONObject;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     public void getWeatherCurrentDay(View view) {
         String tempUrlCurrent = "";
         String cityCurrent = city.getText().toString().trim();
+        DateFormat date = new SimpleDateFormat("dd-MM-yyyy");
         DateFormat timeconvert = new SimpleDateFormat("hh:mm  aa");
         //IF statement to check if the user has entered a city
         if (cityCurrent.equals("")) {
@@ -76,9 +78,12 @@ public class MainActivity extends AppCompatActivity {
                     Date sunset12hr= new Date( epochSunset * 1000 );
                     //Time Convert for current time
                     long epochTime = Long.parseLong(String.valueOf(time));
-                    Date time12hr= new Date( epochTime * 1000 );
+                    Date day= new Date( epochTime * 1000 );
+                    Calendar c = Calendar.getInstance();
+                    c.setTime(day);
+                    String dayText = new SimpleDateFormat("EEEE").format(day);
 
-                    output += "Current weather of " + cityName + " at " + timeconvert.format(time12hr)
+                    output +=  dayText + " " + date.format(day)
                             + "\n Min Temperature: " + df.format(minTemp) + " °C"
                             + "\n Max Temperature: " + df.format(maxTemp) + " °C"
                             + "\n Sunrise: " + timeconvert.format(sunrise12hr)
@@ -122,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray jsonArray = jsonResponse.getJSONArray("list");
                     //Information for day 1
                     JSONObject dailyForecast = jsonArray.getJSONObject(0);
+                    int time = dailyForecast.getInt("dt");
                     JSONObject jsonObjectMain = dailyForecast.getJSONObject("main");
                     double minTemp = jsonObjectMain.getDouble("temp_min") - 273.15;
                     double maxTemp = jsonObjectMain.getDouble("temp_max") - 273.15;
@@ -129,7 +135,8 @@ public class MainActivity extends AppCompatActivity {
                     double speed = jsonObjectWind.getInt("speed");
                     double direction = jsonObjectWind.getInt("deg");
                     //Information for day 2
-                    JSONObject dailyForecast2 = jsonArray.getJSONObject(1);
+                    JSONObject dailyForecast2 = jsonArray.getJSONObject(7);
+                    int time2 = dailyForecast2.getInt("dt");
                     JSONObject jsonObjectMain2 = dailyForecast2.getJSONObject("main");
                     double minTemp2 = jsonObjectMain2.getDouble("temp_min") - 273.15;
                     double maxTemp2 = jsonObjectMain2.getDouble("temp_max") - 273.15;
@@ -137,7 +144,8 @@ public class MainActivity extends AppCompatActivity {
                     double speed2 = jsonObjectWind2.getInt("speed");
                     double direction2 = jsonObjectWind2.getInt("deg");
                     //Information for day 3
-                    JSONObject dailyForecast3 = jsonArray.getJSONObject(2);
+                    JSONObject dailyForecast3 = jsonArray.getJSONObject(15);
+                    int time3 = dailyForecast3.getInt("dt");
                     JSONObject jsonObjectMain3 = dailyForecast3.getJSONObject("main");
                     double minTemp3 = jsonObjectMain3.getDouble("temp_min") - 273.15;
                     double maxTemp3 = jsonObjectMain3.getDouble("temp_max") - 273.15;
@@ -145,7 +153,8 @@ public class MainActivity extends AppCompatActivity {
                     double speed3 = jsonObjectWind3.getInt("speed");
                     double direction3 = jsonObjectWind3.getInt("deg");
                     //Information for day 4
-                    JSONObject dailyForecast4 = jsonArray.getJSONObject(3);
+                    JSONObject dailyForecast4 = jsonArray.getJSONObject(23);
+                    int time4 = dailyForecast4.getInt("dt");
                     JSONObject jsonObjectMain4 = dailyForecast4.getJSONObject("main");
                     double minTemp4 = jsonObjectMain4.getDouble("temp_min") - 273.15;
                     double maxTemp4 = jsonObjectMain4.getDouble("temp_max") - 273.15;
@@ -153,7 +162,8 @@ public class MainActivity extends AppCompatActivity {
                     double speed4 = jsonObjectWind4.getInt("speed");
                     double direction4 = jsonObjectWind4.getInt("deg");
                     //Information for day 5
-                    JSONObject dailyForecast5 = jsonArray.getJSONObject(4);
+                    JSONObject dailyForecast5 = jsonArray.getJSONObject(31);
+                    int time5 = dailyForecast5.getInt("dt");
                     JSONObject jsonObjectMain5 = dailyForecast5.getJSONObject("main");
                     double minTemp5 = jsonObjectMain5.getDouble("temp_min") - 273.15;
                     double maxTemp5 = jsonObjectMain5.getDouble("temp_max") - 273.15;
@@ -161,32 +171,62 @@ public class MainActivity extends AppCompatActivity {
                     double speed5 = jsonObjectWind5.getInt("speed");
                     double direction5 = jsonObjectWind5.getInt("deg");
 
+                    long epochTime = Long.parseLong(String.valueOf(time));
+                    Date day= new Date( epochTime * 1000 );
+                    Calendar c = Calendar.getInstance();
+                    c.setTime(day);
+                    String dayText = new SimpleDateFormat("EEEE").format(day);
 
-                    output += "Day 1"
+                    long epochTime2 = Long.parseLong(String.valueOf(time2));
+                    Date day2= new Date( epochTime2 * 1000 );
+                    Calendar c2 = Calendar.getInstance();
+                    c2.setTime(day2);
+                    String dayText2 = new SimpleDateFormat("EEEE").format(day2);
+
+                    long epochTime3 = Long.parseLong(String.valueOf(time3));
+                    Date day3= new Date( epochTime3 * 1000 );
+                    Calendar c3 = Calendar.getInstance();
+                    c3.setTime(day3);
+                    String dayText3 = new SimpleDateFormat("EEEE").format(day3);
+
+                    long epochTime4 = Long.parseLong(String.valueOf(time4));
+                    Date day4= new Date( epochTime4 * 1000 );
+                    Calendar c4 = Calendar.getInstance();
+                    c4.setTime(day4);
+                    String dayText4 = new SimpleDateFormat("EEEE").format(day4);
+
+                    long epochTime5 = Long.parseLong(String.valueOf(time5));
+                    Date day5= new Date( epochTime5 * 1000 );
+                    Calendar c5 = Calendar.getInstance();
+                    c5.setTime(day5);
+                    String dayText5 = new SimpleDateFormat("EEEE").format(day5);
+
+
+                    output += dayText
                             +"\nMin Temperature: " + df.format(minTemp) + " °C"
                             + "\nMax Temperature: " + df.format(maxTemp) + " °C"
                             + "\nWind Speed: " + speed + " mph"
                             + "\nWind Direction: " + direction + " °"
                             + "\n"
-                            + "\nDay 2"
+                            + "\n" + dayText2
                             + "\nMin Temperature: " + df.format(minTemp2) + " °C"
                             + "\nMax Temperature: " + df.format(maxTemp2) + " °C"
                             + "\nWind Speed: " + speed2 + " mph"
                             + "\nWind Direction: " + direction2 + " °"
                             + "\n"
-                            + "\nDay 3"
+                            + "\n" + dayText3
                             + "\nMin Temperature: " + df.format(minTemp3) + " °C"
                             + "\nMax Temperature: " + df.format(maxTemp3) + " °C"
                             + "\nWind Speed: " + speed3 + " mph"
                             + "\nWind Direction: " + direction3 + " °"
                             + "\n"
-                            + "\nDay 4"
+                            + "\n" + dayText4
                             + "\nMin Temperature: " + df.format(minTemp4) + " °C"
                             + "\nMax Temperature: " + df.format(maxTemp4) + " °C"
                             + "\nWind Speed: " + speed4 + " mph"
                             + "\nWind Direction: " + direction4 + " °"
                             + "\n"
-                            + "\nDay 5"
+                            + "\n" + dayText5
                             + "\nMin Temperature: " + df.format(minTemp5) + " °C"
                             + "\nMax Temperature: " + df.format(maxTemp5) + " °C"
                             + "\nWind Speed: " + speed5 + " mph"
